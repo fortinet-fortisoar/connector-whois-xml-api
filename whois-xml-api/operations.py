ebd3375a9d1088f544db6546e4d06203eaebc5c5
cs-sujeet-ahirrao <sahirrao@fortinet.com>
2023-06-23 11:31:17 +0530

    Incorporated Review Comments
    Incorporated Review Comments

--- whois-xml-api/operations.py
+++ whois-xml-api/operations.py
@@ -4,32 +4,11 @@
   FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
   Copyright end """
 
-import validators, requests, json
-import base64
+import requests, json
+from .constants import *
 from connectors.core.connector import get_logger, ConnectorError
 
 logger = get_logger('whois-xml-api')
-
-True_False_Map = {
-    "True": 1,
-    "False": 0
-}
-Domain_Availability_Map = {
-    "0 - Available": 0,
-    "1 - Unavailable": 1,
-    "2- Undetermined": 2
-}
-WHOISXML_WHOIS_HISTORY_SEARCH_ENDPOINT = 'https://whois-history.whoisxmlapi.com/api/v1'
-WHOISXML_WHOIS_SEARCH_ENDPOINT = 'https://www.whoisxmlapi.com/whoisserver/WhoisService'
-WHOISXML_REVERSE_WHOIS_SEARCH_ENDPOINT = 'https://reverse-whois.whoisxmlapi.com/api/v2'
-WHOISXML_REVERSE_DNS_SEARCH_ENDPOINT = {'IPv4 Address': 'https://dns-history.whoisxmlapi.com/api/v1',
-                                        'Mail Server': 'https://reverse-mx.whoisxmlapi.com/api/v1',
-                                        'Name Server': 'https://reverse-ns.whoisxmlapi.com/api/v1'}
-WHOISXML_DNS_LOOKUP_ENDPOINT = 'https://www.whoisxmlapi.com/whoisserver/DNSService?apiKey={}&domainName={}&outputFormat=JSON&type={}'
-WHOISXML_DOMAIN_SUBDOMAIN_DISCOVERY_ENDPOINT = 'https://domains-subdomains-discovery.whoisxmlapi.com/api/v1'
-WHOISXML_API_TOKEN_ENDPOINT = 'https://reverse-ip.whoisxmlapi.com/api/v1/?apiKey={}&ip=8.8.8.8'
-WHOISXML_BRAND_MONITOR_ENDPOINT = 'https://brand-alert.whoisxmlapi.com/api/v2'
-WHOISXML_SSL_CERTIFICATES_ENDPOINT = 'https://ssl-certificates.whoisxmlapi.com/api/v1'
 
 
 class WhoisXMLAPI(object):
@@ -49,6 +28,8 @@ class WhoisXMLAPI(object):
                     return response.json()
                 else:
                     return response.content
+            elif response.status_code == 404:
+                return response.text
             else:
                 logger.error(response.content)
                 raise ConnectorError(
@@ -123,7 +104,8 @@ def reverse_dns_search(config, params):
         wxa = WhoisXMLAPI(config)
         params.update({'apiKey': config.get('api_key'), 'outputFormat': 'JSON'})
         params = get_params(params)
-        return wxa.make_api_call(endpoint=WHOISXML_REVERSE_DNS_SEARCH_ENDPOINT.get(params.get('reverse_dns_type')), params=params)
+        return wxa.make_api_call(endpoint=WHOISXML_REVERSE_DNS_SEARCH_ENDPOINT.get(params.get('reverse_dns_type')),
+                                 params=params)
     except Exception as err:
         logger.exception(str(err))
         raise ConnectorError(str(err))
